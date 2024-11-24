b21f86141694392627b88678a398a7e338fe2698
pankaj Yadav <pankajyadav851685@gmail.com>
2024-11-24 09:05:30 +0530

    responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
   return (
     <div className="pb-4 lg:mb-36">
         <div className="flex flex-wrap lg:flex-row-reverse">
-            <div className="w-full lg:w-1/3">
+            <div className="w-full lg:w-1/2">
                 <div className="flex justify-center lg:p-8">
                  <motion.img 
                  src={profilePic} 
@@ -37,7 +37,7 @@ const Hero = () => {
                  transition={{ duration: 1, delay: 1.5}} />
                 </div>
             </div>
-            <div className="w-full lg:w-1/3">
+            <div className="w-full lg:w-1/2">
                  
                 <motion.div 
                    initial="hidden"
