ab11fd03420fa035326f0b061d86e1d40abb4060
pankaj Yadav <pankajyadav851685@gmail.com>
2024-11-24 08:43:06 +0530

    responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
   return (
     <div className="pb-4 lg:mb-36">
         <div className="flex flex-wrap lg:flex-row-reverse">
-            <div className="w-full lg:w-1/3">
+            <div className="w-full lg:w-1/2">
                 <div className="flex justify-center lg:p-8">
                  <motion.img 
                  src={profilePic} 
